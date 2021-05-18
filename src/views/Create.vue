707e3aaf8161312dd9a2a46f3ec5e7fd7fd7be14
mizucoffee <develop@mizucoffee.net>
2021-05-18 12:25:29 +0900

    [add] top page

---
+++ src/views/Create.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="about">
+    <section class="hero is-dark">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="tokumasa-title title" @click="$router.push('/')">
+            席替えTokumasa
+          </h1>
+          <h2 class="subtitle is-6">クラスデータ作成ページ</h2>
+        </div>
+      </div>
+    </section>
+
+    <div class="container my-4">
+      <h3 class="title is-3">机辺りの人数</h3>
+
+      <img src="/table.png" alt="" width="600px" style="margin: auto; display: block;" class="my-4"/>
+      
+      <div class="columns">
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+        <div class="column">
+          <b-numberinput v-model="number"></b-numberinput>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <section class="hero is-dark">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="tokumasa-title title">席替えTokumasa</h1>
+          <h2 class="subtitle is-6">トップページ</h2>
+        </div>
+      </div>
+    </section>
+
+    <div class="container my-4">
+      <section class="section">
+        <h2 class="title is-2">メインメニュー</h2>
+        <div class="columns">
+          <div class="column">
+            <input type="file" class="filepond" id="class_data" onchange="myFunction(e)" accept=".osakascs">
+            <b-button id="start_btn" type="is-primary is-large is-fullwidth" disabled>席替えページへ</b-button>
+          </div>
+          <div class="column">
+            <b-button type="is-large is-fullwidth" @click="$router.push('create')">クラスデータ生成ページへ</b-button>
+          </div>
+        </div>
+      </section>
+      <section class="section">
+        <h2 class="title is-2">使い方</h2>
+        <div class="block">
+          <h3 class="title is-3">ステップ1 - クラスデータを準備する</h3>
+          <p>
+            席替えTokumasaではクラスの情報を1つのファイル（.osakascs）にまとめて使用します。<br>
+            このデータには座席に座るメンバーの写真や、人数、テーブル辺りの人数などが記録されています。<br>
+            <br>
+            <a href="/create.html">クラスデータ生成ページ</a>へ移動し、手順に従って生成してください。
+          </p>
+        </div>
+        <div class="block">
+          <h3 class="title is-3">ステップ2 - 席替えをする</h3>
+          <p>
+            ステップ1で生成したクラスデータを元にランダムに席順を決めます。<br>
+            席替えページヘをクリックすると、クラスデータ（.osakascs）を選択する画面が出てくるので選択してください。<br>
+            ファイルを選択すると自動的にランダムページに移動するので、Enterキーを押すと確定します。<br>
+            また、確定と同時にファイルを画像ファイルをダウンロードするように誘導されるのでダウンロードしておいてください。
+          </p>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class Home extends Vue {}
 </script>
